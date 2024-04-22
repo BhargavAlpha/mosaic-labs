f41b93c340518a1b20a85aec9959c1a6c2683584
Bhargav <bhargavvenkat515@gmail.com>
2024-04-22 23:28:55 +0530

    Added same text to newly created div

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("Button clicked!");
         const executedQueryDiv = document.createElement("div");
         executedQueryDiv.className = "lorem-content";
-        executedQueryDiv.innerHTML = `Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet, consectetur<i class="fa fa-spinner fa-spin tick-icon" style="color: #F5461B;"></i>`;
+        executedQueryDiv.innerHTML = `Lorem ipsum dolor sit amet, consectetur.Lorem ipsum dolor sit amet, consectetur<i class="fa fa-spinner fa-spin tick-icon" style="color: #F5461B;"></i>`;
 
         contentDiv.insertBefore(executedQueryDiv, contentDiv.firstChild);
         
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
             if (tickIcon) {
                 tickIcon.className = "fa fa-check-circle tick-icon";
                 tickIcon.style.color = "rgb(100, 212, 100)";
-                executedQueryDiv.innerHTML = `Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur<i class="fa fa-check-circle tick-icon" style="color: rgb(100, 212, 100);"></i>`;
+                // executedQueryDiv.innerHTML = `Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur<i class="fa fa-check-circle tick-icon" style="color: rgb(100, 212, 100);"></i>`;
             }
         }, 10000);
     });
